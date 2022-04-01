60f6f979e6ee5add73b8223fe9598ad9ecbe3e38
Mohammad Abu-Shelbaia <mabushelbaia@gmail.com>
2022-04-02 00:12:59 +0300

    Update MergeSort.cpp

--- Sorting Algorthims/MergeSort.cpp
+++ Sorting Algorthims/MergeSort.cpp
@@ -38,28 +38,7 @@ void merge(int arr[], int p, int q, int r) {
         arr[k++] = M[j++];
 
 }
-// void merge(int arr[],int l,int m, int r){
-// }
-//   int i=l; // starting index for left sub-array
-//   int j=m+1; //starting index for right sub-array
-//   int k=l;    // starting index for temp
-//   int temp[r-l+1];
-//   while(i<=m && j<=r){
-//     if(arr[i] <= arr[j]){
-//       temp[k++] = arr[i++]; // arr[i] is smaller than of arr[j]
-//       continue;
-//     }
-//     temp[k++] = arr[j++];
-//   }
-//
-//   while(i<=m)
-//     temp[k++] = arr[i++];
-//   while(j<=r)
-//     temp[k++] = arr[j++];
-//   for (int s = l; s <= r; s++){
-//     arr[s] = temp[s];
-//   }
-// }
+
 void mergeSort(int arr[],int l,int r){
     if(l < r){
       int m = l + (r-l)/2;
